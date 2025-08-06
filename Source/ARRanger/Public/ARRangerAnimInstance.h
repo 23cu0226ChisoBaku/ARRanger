ae7c69cd8d9f11cdff7b78aad50369567d14d673
0340RikuYoshida <23cu0340@jec.ac.jp>
2025-08-06 18:57:12 +0900

    Adjusted Attraction-Climbing Animation and Fixed Some Actions

--- Source/ARRanger/Public/ARRangerAnimInstance.h
+++ Source/ARRanger/Public/ARRangerAnimInstance.h
@@ -10,9 +10,15 @@ class ARRANGER_API UARRangerAnimInstance : public UAnimInstance
 	GENERATED_BODY()
 	
 public:
+    // 引力クライム時の上下方向の速度
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Climb")
     float ClimbUpSpeed;
 
+    // 引力クライム時の左右方向の速度
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Climb")
     float ClimbRightSpeed;
+
+    // 引力クライム中フラグ
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Climb")
+    bool IsClimbing;
 };
