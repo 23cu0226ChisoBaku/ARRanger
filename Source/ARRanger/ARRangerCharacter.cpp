421d6649ee4567dbeb116e8b24c11231980107da
student <>
2025-09-18 14:58:25 +0900

    Back Up By Yoshida

--- Source/ARRanger/ARRangerCharacter.cpp
+++ Source/ARRanger/ARRangerCharacter.cpp
@@ -184,7 +184,7 @@ void AARRangerCharacter::Tick(float DeltaTime)
 	// 引力クライム中に処理
 	if (bIsClimbed)
 	{
-		const float ClimbSpeed = 700.0f; // 上昇速度
+		const float ClimbSpeed = 2100.0f; // 上昇速度
 		AddActorWorldOffset(FVector(0, 0, ClimbSpeed * DeltaTime), false);
 
 		// 壁回転処理
@@ -354,14 +354,14 @@ void AARRangerCharacter::StartClimbing(AInsekiClimbingObject* ClimbActor)
 		wallNormal = HitResult.ImpactNormal;
 
 		// UpをwallNormalにする
-		FVector Up = wallNormal;
+		//FVector Up = wallNormal;
 
-		// 前方向を作成（Up とワールド右ベクトルから計算）
-		FVector Forward = FVector::CrossProduct(Up, FVector::RightVector).GetSafeNormal();
+		//// 前方向を作成（Up とワールド右ベクトルから計算）
+		//FVector Forward = FVector::CrossProduct(Up, FVector::RightVector).GetSafeNormal();
 
-		// 回転を作成
-		const FRotator NewRot = FRotationMatrix::MakeFromXZ(Forward, Up).Rotator();
-		SetActorRotation(NewRot);
+		//// 回転を作成
+		//const FRotator NewRot = FRotationMatrix::MakeFromXZ(Forward, Up).Rotator();
+		//SetActorRotation(NewRot);
 	}
 }
 
