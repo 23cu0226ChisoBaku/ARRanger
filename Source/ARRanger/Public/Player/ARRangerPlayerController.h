07b696cebaee9a5b5601eeb0299cd5f33e4b3c50
麦 <23cu0226@jec.ac.jp>
2025-10-23 23:00:29 +0900

    no message

--- Source/ARRanger/Public/Player/ARRangerPlayerController.h
+++ Source/ARRanger/Public/Player/ARRangerPlayerController.h
@@ -1,4 +1,7 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+/**
+ * @file ARRangerPlayerController.h
+ * @brief Player controller for ARRanger project
+ */
 
 #pragma once
 
@@ -8,6 +11,7 @@
 
 #include "ARRangerPlayerController.generated.h"
 
+/**Forward declaration */
 class UInputMappingContext;
 class UARInputMappingContext;
 class UARAbilitySystemComponent;
@@ -21,9 +25,11 @@ struct FInputActionValue;
 
 #define UE_API ARRANGER_API
 
+/**
+ * @brief Handle class for input hold Game ability 
+ */
 struct FGA_HoldHandle
 {
-
   friend class AARRangerPlayerController;
 
   UE_API static const FGA_HoldHandle InvalidHandle;
@@ -44,6 +50,10 @@ private:
 
 };
 
+/**
+ * @brief Hold handle for Blueprint
+ * 
+ */
 USTRUCT(BlueprintType, Blueprintable)
 struct FGABlueprintableHoldHandle
 {
@@ -61,9 +71,9 @@ class AARRangerPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
-public:
-
   DECLARE_DELEGATE_RetVal_TwoParams(FGameplayTag, FOnGameplayAbilityHeld, float, const FGameplayTag&);
+public:
+  // Delegate for Game ablity holding event
   FOnGameplayAbilityHeld OnGameAbilityHeld;
 
 protected:
@@ -87,21 +97,50 @@ public:
   UE_API virtual void EndPlay(const EEndPlayReason::Type EndReason) override;
   /**End AActor Interface */
 
+  /**Get ARRanger Ability system component */
   UFUNCTION(BlueprintPure, Category = "ARRanger|PlayerController", meta = (DisplayName = "Get ARAbilitySystemComponent"))
   UE_API UARAbilitySystemComponent* GetARASC() const;
 
+  /**
+   * @brief This function will be called if Gameplay ability is activated
+   * @param InNextIMCTag Tag of next input mapping context
+   */
   UFUNCTION(BlueprintCallable, Category = "ARRanger|PlayerController")
   UE_API void OnGameplayAbilityActivate(const FGameplayTag& InNextIMCTag);
   
+  /**
+   * @brief This function will be called if Gameplay ability is ended
+   * @param bWasCanceled Flag indicates that ability was canceled or not
+   */
   UFUNCTION(BlueprintCallable, Category = "ARRanger|PlayerController")
   UE_API void OnGameplayAbilityEnd(bool bWasCanceled);
   
+  /**
+   * @brief This function will be called if an input hold gameplay ability is activated 
+   * @param InActivatedAbilityTag   Tag of activated gameplay ability
+   * @param bBlockInputTag          Block input that contains tag if true
+   * @param InInputBlockIgnoreTags  Tags to ignore block statement.Only use if bBlockInputTag is true
+   * 
+   * @return FGABlueprintableHoldHandle Hold handle that can store in blueprint. Invalid handle if bBlockInputTag is false
+   */
   UFUNCTION(BlueprintCallable, Category = "ARRanger|PlayerController")
   [[nodiscard]] UE_API FGABlueprintableHoldHandle OnGameplayAbilityActivated_Hold(FGameplayTag InActivatedAbilityTag, bool bBlockInputTag, FGameplayTagContainer InInputBlockIgnoreTags);
 
+  /**
+   * @brief This function will be called if an input hold gameplay ability is ticking task
+   * @param InTaskOwnerAbilityTag Tag of task owner gameplay ability
+   * @param HeldTime Interval time that has been held from beginning
+   * @param DeltaTime Time between current and previous TickTask() called 
+   */
   UFUNCTION(BlueprintCallable, Category = "ARRanger|PlayerController")
   UE_API void OnGameplayAbilityTaskTicked_Holding(FGameplayTag InTaskOwnerAbilityTag, float HeldTime, float DeltaTime);
   
+  /**
+   * @brief This function will be called if an input hold gameplay ability is ended 
+   * @param InEndedAbilityTag     Tag of ended gameplay ability
+   * @param InHandle              Handle that created by OnGameplayAbilityActivated_Hold. @see OnGameplayAbilityActivated_Hold()
+   * @param TimeHeld              Total time of an input hold gameplay ability from activating to ending
+   */
   UFUNCTION(BlueprintCallable, Category = "ARRanger|PlayerController")
   UE_API void OnGameplayAbilityEnded_Hold(FGameplayTag InEndedAbilityTag, FGABlueprintableHoldHandle InHandle, float TimeHeld);
 
@@ -126,16 +165,19 @@ protected:
   UE_API bool IsInputBlocked(const FGameplayTag& InInputTag) const;
   
 private:
+
   void SwitchNextIMC(const FGameplayTag& InNextIMCTag);
-
   void Initialize();
-
   void InitializePlayerInputBuffer();
 
+  /**Start gameplay ability input callback function */
   void AbilityInputTagPressed(FGameplayTag InInputTag, bool bOverrideInputState = true);
   void AbilityInputTagReleased(FGameplayTag InInputTag, bool bOverrideInputState = true);
+  /**End gameplay ability input callback function */
+
   void EvaluateInputBuffer(const float DeltaTime, const bool bGamePaused);
 
+  /**Start native input callback function */
   void NativeInput_Move(const FInputActionValue& InputActionValue, /**PayLoad */ FGameplayTag InInputTag);
   void NativeInput_ToggleLockOn(const FInputActionValue& InputActionValue, /**PayLoad */ FGameplayTag InInputTag);
   void NativeInput_SwitchTarget_Right(const FInputActionValue& InputActionValue, /**PayLoad */ FGameplayTag InInputTag);
@@ -144,7 +186,8 @@ private:
   void NativeInput_ChargeRotate(const FInputActionValue& InputActionValue, /**PayLoad */ FGameplayTag InInputTag);
   void NativeInput_TargetSnap(const FInputActionValue& InputActionValue, /**PayLoad */ FGameplayTag InInputTag);
   void NativeInput_ResetCamera(const FInputActionValue& InputActionValue, /**PayLoad */ FGameplayTag InInputTag);
-
+  /**End native input callback function */
+  
 private:
 
   UPROPERTY(EditDefaultsOnly, Category = "ARInput|InputConfig")
